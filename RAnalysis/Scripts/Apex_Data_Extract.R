4b16824b29c55fb8a49555c2793fec4b539dc4e9
Hollie Putnam <hollieputnam@gmail.com>
2018-06-04 03:32:54 -0400

    updated code to extract apex data from instrument

--- RAnalysis/Scripts/Apex_Data_Extract.R
+++ RAnalysis/Scripts/Apex_Data_Extract.R
@@ -1,33 +1,24 @@
 #http://166.122.78.194:80/cgi-bin/datalog.xml?sdate=1806030000&days=2
 #http://www.informit.com/articles/article.aspx?p=2215520
 
-install.packages("XML")
-install.packages("plyr")
-install.packages("ggplot2")
-install.packages("gridExtra")
-
-require("XML")
-require("plyr")
-require("ggplot2")
-require("gridExtra")
 
 
-xmlfile <- xmlParse("http://166.122.78.194:80/cgi-bin/datalog.xml?sdate=1806010000&days=3") #read in the last 2 days of Apex data
+library("XML")
+
+xmlfile <- xmlParse("http://166.122.78.194:80/cgi-bin/datalog.xml?sdate=1806010000&days=3") #read in the date plus x days of Apex data
 
 Apex.Data <- ldply(xmlToList(xmlfile), data.frame) #convert xml to dataframe
 
-Apex.Data2 <- Apex.Data[4:nrow(Apex.Data),]
-Apex.Data2 <- head(Apex.Data2,-2)
-  
-#Apex.Data2[c(1:nrow(Apex.Data2)-2,)]
+Apex.Data2 <- Apex.Data[4:nrow(Apex.Data),] #remove extra metadata from top
+Apex.Data2 <- head(Apex.Data2,-2) #remove extra metadata from bottom
 
-#keep columnes with 
-Probe.Data <- Apex.Data2[,c(3,6,9,15, 75, 78, 72, 81, 84, 90)]
-colnames(Probe.Data ) <- c("Date.Time", "Tmp_XL", "pH_XL", "Salt_XL", "Tmp_L", "pH_L", "Salt_L","Tmp_A", "pH_A", "Salt_A")  
-Probe.Data$Date.Time <- as.POSIXct(Probe.Data$Date.Time, format = "%m/%d/%Y %H:%M:%S", tz="HST")
-write.csv(Probe.Data, "~/MyProjects/BioMin_HIS/RAnalysis/Output/Apex_Data_Output.csv")
+#keep columnes with data of interest. This needs to be changed as it will be specific to the Apex configuration
+Probe.Data <- Apex.Data2[,c(3,6,9,15, 75, 78, 72, 81, 84, 90)] #select columns
+colnames(Probe.Data ) <- c("Date.Time", "Tmp_XL", "pH_XL", "Salt_XL", "Tmp_L", "pH_L", "Salt_L","Tmp_A", "pH_A", "Salt_A")  #rename columns
+Probe.Data$Date.Time <- as.POSIXct(Probe.Data$Date.Time, format = "%m/%d/%Y %H:%M:%S", tz="HST") #convert date to HI time
+write.csv(Probe.Data, "~/MyProjects/BioMin_HIS/RAnalysis/Output/Apex_Data_Output.csv") #write file to save data
 
-
+#plot Temp and pH and save to output
 pdf("~/MyProjects/BioMin_HIS/RAnalysis/Output/Apex_Output.pdf")
 par(mfrow=c(2,1))
 plot(as.numeric(as.character(Tmp_XL)) ~ Date.Time, Probe.Data, col = "grey", type="l", ylim=c(25.5, 28),  xlab="Time", ylab="Temperature °C")
