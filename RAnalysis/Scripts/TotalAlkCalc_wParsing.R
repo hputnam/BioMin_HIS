4429f2327f762fae8f29a57263b5e027d2981e0a
Hollie Putnam <hollieputnam@gmail.com>
2018-06-19 21:06:26 -0400

    updated seawater chemistry data and output

--- RAnalysis/Scripts/TotalAlkCalc_wParsing.R
+++ RAnalysis/Scripts/TotalAlkCalc_wParsing.R
@@ -23,12 +23,12 @@ library(seacarb) #used to calculate TA
 library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
-path<-"Data/20180613" #the location of all your titration files
-massfile<-"20180613mass_Run1.csv" # name of your file with masses
-titrationfile<-'20180613_Run1.csv'# name of the last titration file run
+path<-"Data/20180619" #the location of all your titration files
+massfile<-"20180619mass_Run1.csv" # name of your file with masses
+titrationfile<-'20180619_Run1.csv'# name of the last titration file run
 
 # Date that the data were run
-date<-'20180613'
+date<-'20180619'
 
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS A NEW BOTTLE OF ACID IS USED
